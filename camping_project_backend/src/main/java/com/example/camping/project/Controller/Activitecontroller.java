889266710d6eb1bdddfa8ba1c03a3533dde69a8c
yassinkouki <koukiyassin5@gmail.com>
2024-07-03 17:50:22 +0100

    je ajouter dans ce classe le autorisation de user et admin

--- camping_project_backend/src/main/java/com/example/camping/project/Controller/Activitecontroller.java
+++ camping_project_backend/src/main/java/com/example/camping/project/Controller/Activitecontroller.java
@@ -3,6 +3,7 @@ package com.example.camping.project.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -19,46 +20,53 @@ import com.example.camping.project.Service.Activiteservice;
 
 import jakarta.validation.Valid;
 
-
 @RestController
+@PreAuthorize("hasAnyRole('ADMIN','USER')")
 public class Activitecontroller {
 
   @Autowired
   Activiteservice actserv;
 
   @PostMapping("/addActivite")
+  @PreAuthorize("hasAuthority('WRITE_PRIVILEGE') and hasRole('ADMIN')")
   public Activite addActivite(@RequestBody Activite A) {
     return actserv.addActivite(A);
   }
 
   @PutMapping("/updateActivite/{id}")
+  @PreAuthorize("hasAuthority('UPDATE_PRIVILEGE') and hasRole('ADMIN')")
   public Activite updateActivite(@Valid @RequestBody Activite activ, @PathVariable("id") int id_activite) {
     return actserv.updateActivite(activ, id_activite);
   }
 
   @DeleteMapping("/deleteActivite/{id}")
+  @PreAuthorize("hasAuthority('DELETE_PRIVILEGE') and hasRole('ADMIN')")
   public void deleteActivite(@PathVariable("id") Activite actv) {
     actserv.deleteActivite(actv);
   }
 
-
   @GetMapping("/getallActivite")
+  @PreAuthorize("hasAnyRole('ADMIN', 'USER') and hasAuthority('READ_PRIVILEGE')")
   public List<Activite> getallActivite() {
     return actserv.getallActivite();
   }
 
   @GetMapping("/getallActivitebynom/{ch}")
-  public String  getallActivitebynom(@PathVariable("ch") String ch) {
+  @PreAuthorize("hasAnyRole('ADMIN', 'USER') and hasAuthority('READ_PRIVILEGE')")
+  public String getallActivitebynom(@PathVariable("ch") String ch) {
 
     return actserv.getallActivitebynom(ch);
   }
 
   @GetMapping("/getnombreActivitedispo")
-  public int getnombreActivitedispo(){
-   return actserv.getnombreActivitedispo() ;
-}
-   @PatchMapping("/updateActiviteImage/{id}")
-    public Activite updateActiviteImage(@PathVariable  int id ,@RequestParam("file") MultipartFile file) {
-      return actserv.updateActiviteImage(id, file) ;
-    }
+  @PreAuthorize("hasAnyRole('ADMIN', 'USER') and hasAuthority('READ_PRIVILEGE')")
+  public int getnombreActivitedispo() {
+    return actserv.getnombreActivitedispo();
+  }
+
+  @PatchMapping("/updateActiviteImage/{id}")
+  @PreAuthorize("hasAuthority('UPDATE_PRIVILEGE') and hasRole('ADMIN')")
+  public Activite updateActiviteImage(@PathVariable int id, @RequestParam("file") MultipartFile file) {
+    return actserv.updateActiviteImage(id, file);
+  }
 }
