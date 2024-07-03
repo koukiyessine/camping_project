35bbf11c5cd8699e9f251be66e84bb3f71bb98f5
yassinkouki <koukiyassin5@gmail.com>
2024-07-03 12:06:21 +0100

    authcontroller

---
+++ camping_project_backend/src/main/java/com/example/camping/project/Service/AuthenticationService.java
@@ -0,0 +1,43 @@
+package com.example.camping.project.Service;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.security.core.Authentication;
+import com.example.camping.project.Entities.User;
+import com.example.camping.project.Repository.UserRepository;
+import com.example.camping.project.dto.AuthenticationUserDTO;
+import com.example.camping.project.exception.DuplicateUserException;
+import com.example.camping.project.interfaceservice.IAuthenticationService;
+
+public class AuthenticationService implements IAuthenticationService{
+
+      // Repository to handle User entity persistence
+    private final UserRepository userRepository;
+
+    // Constructor injection for UserRepository
+    public AuthenticationService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User register(User user) throws DuplicateUserException {
+        if (user == null) {
+            return null;
+        }
+        try {
+            // Save the user in the repository
+            return userRepository.save(user);
+        } catch (DataIntegrityViolationException e) {
+            // Handle uniqueness constraint violations
+            throw new DuplicateUserException("User already exists");
+        }
+    }
+
+    @Override
+    public AuthenticationUserDTO login(Authentication authentication) {
+        // Retrieve the user principal from the authentication object after basic authentication
+        User user = (User) authentication.getPrincipal();
+        // Convert the User entity to AuthenticationUserDTO and return it
+        return AuthenticationUserDTO.toAuthenticationUserDTO(user);
+    }
+    
+}
